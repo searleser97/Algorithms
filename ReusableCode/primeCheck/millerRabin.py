4d672fab46251e65a157ab6b08a99e0b77593a40
searleser97 <serchgabriel97@gmail.com>
2017-10-07 15:35:57 -0500

    updated: 07/10/2017 03:35:57

--- ReusableCode/primeCheck/millerRabin.py
+++ ReusableCode/primeCheck/millerRabin.py
@@ -5,18 +5,18 @@ def is_prime(p):
     k = 100
     if p == 2 or p == 3:
         return True
-    if p & 1 == 0 or p == 1:
+    if (p & 1) == 0 or p == 1:
         return False
     phi = p - 1
     d = phi
     r = 0
-    while (d & 1 == 0):
+    while (d & 1) == 0:
         d = int(d >> 1)
         r += 1
     for i in range(k):
         a = randrange(2, p - 2)
         exp = pow(a, d, p)
-        if (exp == 1 or exp == p - 1):
+        if exp == 1 or exp == p - 1:
             continue
         for j in range(r - 1):
             exp = pow(exp, 2, p)
