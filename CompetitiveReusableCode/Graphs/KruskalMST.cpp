b662209c6aaa399adc5680f2fcbc04208b8421b4
searleser97 <serchgabriel97@gmail.com>
2018-04-18 11:30:57 -0500

    Create KruskalMST.cpp

---
+++ CompetitiveReusableCode/Graphs/KruskalMST.cpp
@@ -0,0 +1,18 @@
+typedef int Weight;
+typedef pair<int, int> Edge;
+typedef pair<Weight, Edge> Wedge;
+
+vector<WEdge> Wedges; // gets filled from input;
+
+vector<Wedge> kruskal() {
+    vector<Wedge> mst;
+    sort(Wedges.begin(), Wedges.end());
+    // reverse(Wedges.begin(), Wedges.end());
+    UnionFind uf(n);
+    for (Wedge &wedge : Wedges) {
+        int u = wedge.second.first, v = wedge.second.second;
+        if (!uf.areConnected(u, v))
+            uf.join(u, v), mst.push_back(wedge);
+    }
+    return mst;
+}
