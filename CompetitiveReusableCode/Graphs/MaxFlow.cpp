d49e94cd4487d8f814d09a3346b24194c3f5f716
searleser97 <serchgabriel97@gmail.com>
2018-07-01 09:56:00 -0500

    updated: 01/07/2018 09:56:00 AM

--- CompetitiveReusableCode/Graphs/MaxFlow.cpp
+++ CompetitiveReusableCode/Graphs/MaxFlow.cpp
@@ -58,13 +58,20 @@ bool levelGraph(int s, int t) {
     return level[t] != -1;
 }
 
-int blockingFlow(int u, int t, Num pathMaxFlow, vector<int> &path) {
+int blockingFlow(int u, int t, Num currPathMaxFlow, vector<int> &path) {
     path.push_back(u);
-    if (u == t) return pathMaxFlow;
+    if (u == t) return currPathMaxFlow;
+    // cout << "current node: " << u << " pathMaxFlow: " << currPathMaxFlow << endl;
+    // cout << "adyacents:" << endl;
+    for (int v : ady[u])
+        // cout << "    " << v << " cap: " << cap[u][v] << " " << flow[u][v] << endl;
+    // cout << endl;
     for (int v : ady[u]) {
         Num capleft = cap[u][v] - flow[u][v];
-        if (level[v] == level[u] + 1 && capleft > 0) {
-            pathMaxFlow = blockingFlow(v, t, min(pathMaxFlow, capleft), path);
+        // cout << "capleft: " << capleft << endl;
+        if ((level[v] == (level[u] + 1)) && (capleft > 0)) {
+            // cout << "entro " << currPathMaxFlow << " " << capleft << endl;
+            Num pathMaxFlow = blockingFlow(v, t, min(currPathMaxFlow, capleft), path);
             if (pathMaxFlow > 0) {
                 flow[u][v] += pathMaxFlow;
                 flow[v][u] -= pathMaxFlow;
@@ -78,31 +85,44 @@ int blockingFlow(int u, int t, Num pathMaxFlow, vector<int> &path) {
 Num dinicMaxFlow(int s, int t) {
     if (s == t) return -1;
     Num maxFlow = 0;
-    int l = 0;
-    while (!l) {
-        cout << "level: " << ((l = levelGraph(s, t)) ? "true" : "false") << endl;
-        while (l) {
-            vector<int> v;
-            cout << "flow: " << blockingFlow(s, t, 1 << 30, v) << endl;
-            for (int i = 0; i < v.size(); i++)
-                cout << v[i] << " ";
-            cout << endl;
-            cin >> l;
-        }
-    }
-    
-    // while(levelGraph(s, t))
-    //     while (Num flow = blockingFlow(s, t, 1 << 30))
-    //         maxFlow += flow;
+    // int l = 0;
+    // while (!l) {
+    //     cout << "level: " << ((l = levelGraph(s, t)) ? "true" : "false") << endl;
+    //     for (int i = 0; i < level.size(); i++) {
+    //         cout << "    " << i << " " << level[i] << endl;
+    //     }
+    //     while (l) {
+    //         vector<int> v;
+    //         Num f = blockingFlow(s, t, 1 << 30, v);
+    //         cout << "flow: " << f << endl;
+    //         for (int i = 0; i < v.size(); i++)
+    //             cout << v[i] << " ";
+    //         cout << endl;
+    //         cin >> l;
+    //     }
+    // }
+    vector<int> v;
+    while(levelGraph(s, t))
+        while (Num flow = blockingFlow(s, t, 1 << 30, v))
+            maxFlow += flow;
     return maxFlow;
 }
 
+typedef int Key;
+int nextId = 0;
+
+int Map(Key key, unordered_map<int, int> &id) {
+    return id.count(key) ? id[key] : id[key] = nextId++;
+}
+
 int main() {
-    int s, t, c;
+    int s, t, c, i = 1;
     while (true) {
         cin >> N;
         if (!N)
             return 0;
+        unordered_map<int, int> nodeId;
+        nextId = 0;
         ady.resize(N);
         level.resize(N);
         cap.resize(N, vector<int>(N));
@@ -111,10 +131,14 @@ int main() {
         while (c--) {
             int u, v, c;
             cin >> u >> v >> c;
-            addEdge(u - 1, v - 1, cap[u - 1][v - 1] + c);
-            addEdge(v - 1, u - 1, cap[v - 1][u - 1] + c);
+            u = Map(u, nodeId);
+            v = Map(v, nodeId);
+            addEdge(u, v, cap[u][v] + c);
+            addEdge(v, u, cap[v][u] + c);
         }
+        cout << "Network " << i << endl;
         cout << "The bandwidth is " << dinicMaxFlow(s - 1, t - 1) << "." << endl;
+        i++;
     }
     return 0;
 }
