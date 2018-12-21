8ec47653d07b11ea1a9d2f95791a2f8fa59a6d1b
san <gabrielsanv97@gmail.com>
2018-12-20 20:05:51 -0600

    updated: 20/12/2018 08:05:51 PM

--- Reference/Graphs/Has Cycle.cpp
+++ Reference/Graphs/Has Cycle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 vector<vector<int>> ady, cycles;
 vector<int> vis, cycle;
-bool flag = false, isUndirected = true;
+bool flag = false, isDirected = false;
 int root = -1;
 
 void initVars(int N) {
@@ -15,7 +15,7 @@ void initVars(int N) {
 bool hasCycle(int u, int prev) {
   vis[u] = 1;
   for (auto &v : ady[u]) {
-    if (v == u || vis[v] == 2 || (isUndirected && v == prev)) continue;
+    if (v == u || vis[v] == 2 || (!isDirected && v == prev)) continue;
     if (flag) {
       if (!vis[v]) hasCycle(v, u);
       continue;
@@ -33,20 +33,17 @@ bool hasCycle(int u, int prev) {
 // O(N)
 bool hasCycle() {
   for (int u = 0; u < ady.size(); u++)
-    if (!vis[u]) {
-      cycle.clear();
-      if (hasCycle(u, -1)) cycles.push_back(cycle);
-    }
+    if (!vis[u]) cycle.clear(), hasCycle(u, -1);
   return cycles.size() > 0;
 }
 
 void addEdge(int u, int v) {
   ady[u].push_back(v);
-  ady[v].push_back(u);
+  if (!isDirected) ady[v].push_back(u);
 }
 
 int main() {
-  initVars(7);
+  initVars(11);
   addEdge(0, 4);
   addEdge(4, 1);
   addEdge(4, 2);
@@ -56,6 +53,10 @@ int main() {
   addEdge(4, 5);
   addEdge(4, 6);
   addEdge(6, 5);
+  addEdge(7, 8);
+  addEdge(7, 9);
+  addEdge(8, 9);
+  addEdge(9, 10);
   if (hasCycle()) {
     for (auto cycle : cycles) {
       for (auto u : cycle) {
