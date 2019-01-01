58db4dd1a8cdde2e5147e5b3fb6791652ec86e6d
san <gabrielsanv97@gmail.com>
2019-01-01 13:16:57 -0600

    updated: 01/01/2019 01:16:57 PM

--- Reference/Data Structures/Ranges/Segment Tree Lazy Propagation.cpp
+++ Reference/Data Structures/Ranges/Segment Tree Lazy Propagation.cpp
@@ -19,37 +19,40 @@ void initVars(T n) {
 }
 
 T F(T a, T b) {
-  return a + b;
-  // return __gcd(a, b);
+  // return a + b;
+  return __gcd(a, b);
   // return a * b;
   // return min(a, b);
 }
 
 T kTimesF(T a, T k) {
-  return a * k;  // sum
-  // return a; // min, max, gcd
+  // return a * k;  // sum
+  return a; // min, max, gcd
   // return pow(a, k);  // multiplication
 }
 
 void calc(int i) {
-  if (!u[i]) st[i] = st[i << 1] + st[i << 1 | 1];
+  if (!u[i]) st[i] = F(st[i << 1], st[i << 1 | 1]);
 }
 
 void apply(int i, T val, int k) {
-  st[i] += kTimesF(val, k);
-  if (i < N) d[i] += val, u[i] = 1;
+  st[i] = kTimesF(val, k);
+  if (i < N) d[i] = val, u[i] = 1;
+}
+
+// O(2N)
+void build() {
+  for (int i = N - 1; i > 0; i--) calc(i);
 }
 
 void build(int p) {
-  int k = 2;
-  for (; p > 1; k <<= 1) {
-    p >>= 1;
-    for (int i = p; i >= p; --i) calc(i);
+  while (p > 1) {
+    p >>= 1, calc(p);
   }
 }
 
 void push(int p) {
-  for (int s = H, k = 1 << (H - 1); s > 0; --s, k >>= 1) {
+  for (int s = H, k = 1 << (H - 1); s > 0; s--, k >>= 1) {
     int i = p >> s;
     if (u[i]) {
       apply(i << 1, d[i], k);
@@ -59,7 +62,7 @@ void push(int p) {
   }
 }
 
-// [l, r]
+// [l, r] O(lg(2N))
 void modify(int l, int r, T val) {
   push(l += N);
   push(r += N);
@@ -72,7 +75,7 @@ void modify(int l, int r, T val) {
   build(r0);
 }
 
-// [l, r]
+// [l, r] O(lg(2N))
 T query(int l, int r) {
   push(l += N);
   push(r += N);
@@ -92,26 +95,61 @@ T getValAt(int pos) {
   return st[pos + N];
 }
 
+int MAXN = 500000;
+
+unordered_map<int, int> getPos, rep;
+int ans, ind = 0;
+
 int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    long long int n, C;
-    cin >> n >> C;
-    initVars(n);
-    while (C--) {
-      long long int opc, p, q;
-      T val;
-      cin >> opc >> p >> q;
-      if (q < p) swap(p, q);
-      if (q > N) q = N;
-      if (opc) {
-        cout << query(p - 1, q - 1) << '\n';
-      } else {
-        cin >> val;
-        modify(p - 1, q - 1, val);
+  int q;
+  scanf(" %d", &q);
+  initVars(MAXN);
+  for (int h = 0; h < q; h++) {
+    char o;
+    int x, pos;
+    scanf(" %c %d", &o, &x);
+    if (o == '+') {
+      if (getPos.count(x))
+        pos = getPos[x];
+      else {
+        pos = ind++;
+        getPos[x] = pos;
       }
+      if (!rep[x]++)
+        modify(pos, pos, x);
+    } else {
+      if (!--rep[x])
+        modify(getPos[x], getPos[x], neutro);
     }
+    ans = st[1];
+    if (ans)
+      printf("%d\n", ans);
+    else
+      printf("1\n");
   }
   return 0;
 }
+
+// int main() {
+//   int t;
+//   cin >> t;
+//   while (t--) {
+//     long long int n, C;
+//     cin >> n >> C;
+//     initVars(n);
+//     while (C--) {
+//       long long int opc, p, q;
+//       T val;
+//       cin >> opc >> p >> q;
+//       if (q < p) swap(p, q);
+//       if (q > N) q = N;
+//       if (opc) {
+//         cout << query(p - 1, q - 1) << '\n';
+//       } else {
+//         cin >> val;
+//         modify(p - 1, q - 1, val);
+//       }
+//     }
+//   }
+//   return 0;
+// }
