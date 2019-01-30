f1ac9da5c75e44ec5b1bb3778bbea6dc9dcec238
san <gabrielsanv97@gmail.com>
2019-01-29 18:12:13 -0600

    updated: 29/01/2019 06:12:13 PM

--- Reference/Graphs/Topological Sort (All possible sorts).cpp
+++ Reference/Graphs/Topological Sort (All possible sorts).cpp
@@ -1,63 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+// 4
+// indeg0 = indegree 0
 vector<int> vis, indegree, path;
 vector<vector<int>> ady, toposorts;
-deque<int> nextNodes;
-
+deque<int> indeg0;
+// 5
 void initVars(int n) {
   ady.assign(n, vector<int>());
   vis.assign(n, 0);
   indegree = vis;
 }
-
+// 4
 void addEdge(int u, int v) {
   ady[u].push_back(v);
   indegree[v]++;
 }
-
+// 19
+// O(V!)
 void dfs() {
-  for (int i = 0; i < nextNodes.size(); i++) {
-    int u = nextNodes.front(), count = 0;
-    nextNodes.pop_front();
+  for (int i = 0; i < indeg0.size(); i++) {
+    int u = indeg0.front();
+    indeg0.pop_front();
     path.push_back(u);
     for (auto &v : ady[u])
-      if (!--indegree[v])
-        nextNodes.push_back(v), count++;
-    if (!nextNodes.size()) toposorts.push_back(path);
+      if (!--indegree[v]) indeg0.push_back(v);
+    if (!indeg0.size()) toposorts.push_back(path);
     dfs();
-    for (auto &v : ady[u]) indegree[v]++;
-    while (count--) nextNodes.pop_back();
-    nextNodes.push_back(u);
+    for (int v = ady[u].size() - 1; ~v; v--) {
+      indegree[ady[u][v]]++;
+      if (indeg0.back() == ady[u][v])
+        indeg0.pop_back();
+    }
+    indeg0.push_back(u);
     path.pop_back();
   }
 }
-
+// 6
 // O(V + V!)
 void allToposorts() {
   for (int u = 0; u < ady.size(); u++)
-    if (!indegree[u]) nextNodes.push_back(u);
+    if (!indegree[u]) indeg0.push_back(u);
   dfs();
-}
-
-int main() {
-  initVars(6);
-  addEdge(5, 2);
-  addEdge(5, 0);
-  addEdge(4, 0);
-  addEdge(4, 1);
-  addEdge(2, 3);
-  addEdge(3, 1);
-
-  cout << "All Topological sorts\n";
-
-  allToposorts();
-  for (auto &path : toposorts) {
-    for (auto &i : path) {
-      cout << i << " ";
-    }
-    cout << endl;
-  }
-
-  return 0;
 }
